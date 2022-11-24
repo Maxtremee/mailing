227853923fea6433a133c72f0d27b83342fe9528
Maksymilian Zadka <maksymilian.j.zadka@gmail.com>
2022-11-24 12:34:28 +0100

    feat: allow simple API key auth

--- packages/cli/src/pages/api/render.ts
+++ packages/cli/src/pages/api/render.ts
@@ -1,45 +1,48 @@
-import type { NextApiRequest, NextApiResponse } from "next";
-import { MjmlError } from "mjml-react";
+import type { NextApiRequest, NextApiResponse } from "next"
+import { MjmlError } from "mjml-react"
 
-import renderTemplate from "../../util/renderTemplate";
+import renderTemplate from "../../util/renderTemplate"
+import { validateApiKey } from "src/util/validateApiKey"
 
 type Data = {
-  error?: string; // api error messages
-  html?: string;
-  mjmlErrors?: MjmlError[];
-};
+  error?: string // api error messages
+  html?: string
+  mjmlErrors?: MjmlError[]
+}
 
-export default function handler(
+export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse<Data>
 ) {
-  const { templateName, props } = "GET" === req.method ? req.query : req.body;
+  if (!(await validateApiKey(req, res))) return
+
+  const { templateName, props } = "GET" === req.method ? req.query : req.body
 
   // validate template name
   if (typeof templateName !== "string") {
-    return res.status(403).json({ error: "templateName must be specified" });
+    return res.status(403).json({ error: "templateName must be specified" })
   }
 
   // parse props
-  let parsedProps = {};
+  let parsedProps = {}
   try {
     parsedProps =
       "GET" === req.method
         ? JSON.parse(decodeURIComponent(props as string))
-        : props;
+        : props
   } catch {
     return res
       .status(403)
-      .json({ error: "props could not be parsed from querystring" });
+      .json({ error: "props could not be parsed from querystring" })
   }
 
   const { error, mjmlErrors, html } = renderTemplate(
     templateName.replace(/\.[jt]sx?$/, ""),
     parsedProps
-  );
+  )
 
   if (error) {
-    return res.status(404).json({ error });
+    return res.status(404).json({ error })
   }
-  res.status(200).json({ html, mjmlErrors });
+  res.status(200).json({ html, mjmlErrors })
 }
--- packages/cli/src/util/validateApiKey.ts
+++ packages/cli/src/util/validateApiKey.ts
@@ -1,24 +1,27 @@
-import { NextApiRequest, NextApiResponse } from "next";
-import prisma from "../../prisma";
+import { NextApiRequest, NextApiResponse } from "next"
+import prisma from "../../prisma"
 
 export const apiKeyFromReq = (
   req: NextApiRequest
 ): string | string[] | undefined =>
-  (req.headers && req.headers["x-api-key"]) || req.query.apiKey;
+  (req.headers && req.headers["x-api-key"]) || req.query.apiKey
 
 export async function validateApiKey(
   req: NextApiRequest,
   res: NextApiResponse
 ): Promise<boolean> {
-  if (process.env.NEXT_PUBLIC_MAILING_SKIP_AUTH === "true") return true;
+  if (process.env.NEXT_PUBLIC_MAILING_SKIP_AUTH === "true") return true
 
-  const apiKey = apiKeyFromReq(req);
+  const apiKey = apiKeyFromReq(req)
   if (process.env.MAILING_INTEGRATION_TEST && "testApiKey" === apiKey)
-    return true;
+    return true
   if (typeof apiKey !== "string") {
-    res.status(422).end("expected x-api-key in header or apiKey in query");
-    return false;
+    res.status(422).end("expected x-api-key in header or apiKey in query")
+    return false
   }
+
+  if (process.env.MAILING_API_KEY && apiKey === process.env.MAILING_API_KEY)
+    return true
 
   try {
     await prisma.apiKey.findFirstOrThrow({
@@ -26,12 +29,12 @@ export async function validateApiKey(
         id: apiKey as string,
         active: true,
       },
-    });
+    })
 
-    return true;
+    return true
   } catch {
-    res.status(401).end("API key is not valid");
+    res.status(401).end("API key is not valid")
   }
 
-  return false;
+  return false
 }
